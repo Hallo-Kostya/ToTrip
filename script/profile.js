47588eaf81aeabcb81a21b840508f3ae059a62f9
KomotsKE <alexkomov2005@gmail.com>
2024-10-28 23:20:07 +0500

    Добавления popup изменения данных в профиля

---
+++ script/profile.js
@@ -0,0 +1,7 @@
+document.getElementById('open-popup').onclick = function() {
+    document.getElementById('popup').style.display = 'flex';
+};
+
+document.getElementById('close-popup-button').onclick = function() {
+    document.getElementById('popup').style.display = 'none';
+};
