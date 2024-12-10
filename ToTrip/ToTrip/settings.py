3c1603edda1b418aa78e765cef46cf6ff2c662ba
Александр <av.sanya@yandex.ru>
2024-12-11 00:02:06 +0500

    to search api added filter by category, pagination and fundamentals for full search

--- ToTrip/ToTrip/settings.py
+++ ToTrip/ToTrip/settings.py
@@ -63,7 +63,10 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10, 
 }
+
 
 AUTH_USER_MODEL = 'UsersApp.User'
 
--- ToTrip/apps/SearchApp/views.py
+++ ToTrip/apps/SearchApp/views.py
@@ -5,7 +5,7 @@ from apps.PlaceApp.models import Place, City, District, Country, Region, Categor
 from .serializers import  SearchCitySerializer, SearchCountrySerializer, SearchDistrictSerializer, SearchPlaceSerializer, SearchRegionSerializer
 from django.db.models import Q
 from django.db.models.functions import Lower
-        
+from rest_framework.pagination import PageNumberPagination       
 
 class SearchPlacesAPIView(APIView):
     """метод для получения городов, мест, округов, стран, регионов из бд.
@@ -14,8 +14,14 @@ class SearchPlacesAPIView(APIView):
     производится поиск мест по категории и по заданному городу и поиск дополняется этими местами. Если категории нет в запросе, 
     то в поиск идут просто места из города в запросе. Если же наоборот нет города,
     то поиск дополняется местами по данной категории из других городов."""
+    class CustomPagination(PageNumberPagination):
+        page_size = 10  # Количество объектов на странице
+        page_size_query_param = 'page_size'
+        max_page_size = 100
     def get(self, request):
         query = request.GET.get("query", "")
+        full_search = request.GET.get("full_search", "false").lower() == "true"
+        query_cat = request.GET.get("query_cat", "").lower()
         if not query:
             return Response({"error": "Отправлен пустой запрос"}, status=status.HTTP_400_BAD_REQUEST)
         query=query.lower()
@@ -47,36 +53,52 @@ class SearchPlacesAPIView(APIView):
         )
 
         # Сериализация данных
-        cities_data = SearchCitySerializer(cities, many=True).data
-        if len(places) < 10:
-            category = Category.objects.annotate(lower_name=Lower('name')).filter(
-                Q(lower_name__icontains=query) | 
-                Q(lower_name__in=query_parts)
-            )
-            if category:
-                category=category.values_list('id', flat=True)
-                if cities_data:
+        
+        if query_cat == "":
+            if full_search or len(places)<8:
+                category = Category.objects.annotate(lower_name=Lower('name')).filter(
+                    Q(lower_name__icontains=query) | 
+                    Q(lower_name__in=query_parts)
+                )
+                if category:
+                    category=category.values_list('id', flat=True)
+                    if cities_data:
+                        city_ids = cities.values_list('id', flat=True)
+                        category_places = (Place.objects.filter(
+                            Q(category_id__in = category) &
+                            Q(city_id__in = city_ids)
+                        ))
+                        places=places.union(category_places)
+                    elif not cities_data:
+                        category_places = (Place.objects.filter(
+                            Q(category_id__in = category) 
+                        ))
+                        places=places.union(category_places)
+                elif cities_data and not category:
                     city_ids = cities.values_list('id', flat=True)
-                    category_places = set(Place.objects.filter(
-                        Q(category_id__in = category) &
+                    city_places= (Place.objects.filter(
                         Q(city_id__in = city_ids)
                     ))
-                    places=places.union(category_places)
-                elif not cities_data:
-                    category_places = set(Place.objects.filter(
-                        Q(category_id__in = category) 
+                    places=places.union(city_places)
+        else:
+            category_ids = Category.objects.annotate(lower_name=Lower('name')).filter(
+                    Q(lower_name__icontains=query_cat)
+                ).values_list('id', flat=True)
+            if category_ids.exists():
+                places = (Place.objects.filter(
+                        Q(category_id__in = category_ids)
                     ))
-                    places=places.union(category_places)
-            elif cities_data and not category:
-                city_ids = cities.values_list('id', flat=True)
-                city_places= set(Place.objects.filter(
-                    Q(city_id__in = city_ids)
-                ))
-                places=places.union(city_places)
-        places_data = SearchPlaceSerializer(places, many=True).data
-        districts_data = SearchDistrictSerializer(districts, many=True).data
-        regions_data = SearchRegionSerializer(regions, many=True).data
-        countries_data = SearchCountrySerializer(countries, many=True).data
+        paginator = self.CustomPagination()
+        paginated_cities = paginator.paginate_queryset(cities, request, view=self)
+        paginated_places = paginator.paginate_queryset(places, request, view=self)
+        paginated_regions = paginator.paginate_queryset(regions, request, view=self)
+        paginated_districts = paginator.paginate_queryset(districts, request, view=self)
+        paginated_countries = paginator.paginate_queryset(countries, request, view=self)
+        cities_data = SearchCitySerializer(paginated_cities, many=True).data
+        places_data = SearchPlaceSerializer(paginated_places, many=True).data
+        districts_data = SearchDistrictSerializer(paginated_districts, many=True).data
+        regions_data = SearchRegionSerializer(paginated_regions, many=True).data
+        countries_data = SearchCountrySerializer(paginated_countries, many=True).data
 
         return Response({
             "cities": cities_data,
