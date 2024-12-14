06b3a965d3fb2195ab0993ffc73cab2888d4f3fb
Konstantin <kburduk@gmail.com>
2024-12-14 19:59:40 +0500

    Установка datepicker для pnpm; разработан datechanger для формы создания поездки

--- totrip/src/components/ui/trips/newTripForm.tsx
+++ totrip/src/components/ui/trips/newTripForm.tsx
@@ -3,6 +3,10 @@
 import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 import { TripData } from '@/components/ui/types';
+import DatePicker from 'react-datepicker'; // Импортируем компонент DatePicker из react-datepicker
+import 'react-datepicker/dist/react-datepicker.css'; // Импортируем стили для DatePicker
+import { format } from 'date-fns';
+import { ru } from 'date-fns/locale';
 
 interface TripFormProps {
     isOpen: boolean;
@@ -17,41 +21,84 @@ const TripForm: React.FC<TripFormProps> = ({
     onClose,
     onSubmit,
     initialData,
-    days
+    days: initialDays
 }) => {
     const [tripName, setTripName] = useState(initialData?.tripName || '');
     const [tripPlace, setTripPlace] = useState(initialData?.tripPlace || '');
-    const [tripStart, setTripStart] = useState(initialData?.tripStart || '');
-    const [tripEnd, setTripEnd] = useState(initialData?.tripEnd || '');
+    const [tripStart, setTripStart] = useState<Date | null>(initialData?.tripStart ? new Date(initialData.tripStart) : null);
+    const [tripEnd, setTripEnd] = useState<Date | null>(initialData?.tripEnd ? new Date(initialData.tripEnd) : null);
+    const [days, setDays] = useState(initialDays);
 
     useEffect(() => {
         if (isOpen) {
             const handleKeyDown = (e: KeyboardEvent) => {
                 if (e.key === "Escape") {
-                    onClose();
+                    handleClose();
                 }
             };
             window.addEventListener('keydown', handleKeyDown);
             return () => window.removeEventListener('keydown', handleKeyDown);
         }
-    }, [isOpen, onClose]);
+    }, [isOpen]);
 
-    if (!isOpen) return null;
-
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        const tripData = {
-            tripName,
-            tripPlace,
-            tripStart,
-            tripEnd,
-        };
-
-        onSubmit(tripData);
+    const handleClose = () => {
+        setTripName('');
+        setTripPlace('');
+        setTripStart(null);
+        setTripEnd(null);
+        setDays(initialDays);
         onClose();
     };
 
-    return (
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        if (tripStart && tripEnd && tripEnd < tripStart) {
+            alert('Дата окончания поездки не может быть раньше даты начала.');
+            return;
+        }
+
+        const tripData = {
+            tripName,
+            tripPlace,
+            tripStart: tripStart ? format(tripStart, 'dd MMM', { locale: ru }) : '',
+            tripEnd: tripEnd ? format(tripEnd, 'dd MMM', { locale: ru }) : '',
+        };
+
+        onSubmit(tripData);
+        handleClose();
+    };
+    
+    const updateTripEnd = (startDate: Date | null, daysCount: number) => {
+        if (startDate) {
+            const newEndDate = new Date(startDate);
+            newEndDate.setDate(newEndDate.getDate() + daysCount);
+            setTripEnd(newEndDate);
+        }
+    };
+
+    const handleDaysChange = (newDays: number) => {
+        if (newDays < 0) newDays = 0;
+        setDays(newDays);
+        updateTripEnd(tripStart, newDays);
+    };
+
+    const daysText = (days: number) => {
+        const modulo10 = days % 10;
+        const modulo100 = days % 100;
+        if (modulo10 === 1 && modulo100 !== 11) return `${days} день`;
+        if ([2, 3, 4].includes(modulo10) && ![12, 13, 14].includes(modulo100)) return `${days} дня`;
+        return `${days} дней`;
+    };
+
+    useEffect(() => {
+        if (tripStart && tripEnd) {
+            const timeDiff = tripEnd.getTime() - tripStart.getTime();
+            const dayDifference = Math.ceil(timeDiff / (1000 * 3600 * 24));
+            setDays(dayDifference);
+        }
+    }, [tripEnd, tripStart]);
+
+    return !isOpen ? null : (
         <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-gray-800 bg-opacity-75">
             <div className="max-w-[800px] bg-white p-[48px] rounded-[50px] max-w-md w-full">
                 <form onSubmit={handleSubmit}>
@@ -77,38 +124,40 @@ const TripForm: React.FC<TripFormProps> = ({
                     />
                     <div className="mt-[20px] flex justify-between">
                         <h2 className="text-[36px] font-bold">Длительность поездки</h2>
-                        <div className="flex items-center space-x-2">
-                            <button type="button">
+                        <div className="flex items-center">
+                            <button type="button" onClick={() => handleDaysChange(days - 1)}>
                                 <Image src="/img/circle-minus.svg" width={30} height={30} alt="Уменьшить" />
                             </button>
-                            <p className="text-[30px] font-bold">{days} дней</p>
-                            <button type="button">
+                            <p className="text-[30px] font-bold ml-2 mr-2">{daysText(days)}</p>
+                            <button type="button" onClick={() => handleDaysChange(days + 1)}>
                                 <Image src="/img/circle-plus.svg" width={30} height={30} alt="Увеличить" />
                             </button>
                         </div>
                     </div>
                     <div className="flex gap-[30px] mt-[20px]">
-                        <input
-                            type="text"
-                            value={tripStart}
-                            onChange={(e) => setTripStart(e.target.value)}
-                            name="tripstart"
-                            placeholder="Начало"
-                            required
-                            className="w-full mb-4 p-[13px] border border-gray-300 rounded-[15px]"
+                        <DatePicker
+                            selected={tripStart}
+                            onChange={(date: Date) => {
+                                setTripStart(date);
+                                updateTripEnd(date, days);
+                            }}
+                            dateFormat="dd MMM"
+                            locale={ru}
+                            placeholderText="Начало"
+                            className="w-full  p-[13px] border border-gray-300 rounded-[15px]"
                         />
-                        <input
-                            type="text"
-                            value={tripEnd}
-                            onChange={(e) => setTripEnd(e.target.value)}
-                            name="tripend"
-                            placeholder="Конец"
-                            required
-                            className="w-full mb-4 p-[13px] border border-gray-300 rounded-[15px]"
+                        <p className="my-auto text-[25px]">-</p>
+                        <DatePicker
+                            selected={tripEnd}
+                            onChange={(date: Date) => setTripEnd(date)}
+                            dateFormat="dd MMM"
+                            locale={ru}
+                            placeholderText="Конец"
+                            className="w-full  p-[13px] border border-gray-300 rounded-[15px]"
                         />
                     </div>
                     <div className="flex justify-between gap-4 mt-[30px]">
-                        <button type="button" onClick={onClose} className="bg-gray-500 text-white px-4 py-2 rounded-[11px]">Отменить</button>
+                        <button type="button" onClick={handleClose} className="bg-gray-500 text-white px-4 py-2 rounded-[11px]">Отменить</button>
                         <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded-[11px]">Создать поездку</button>
                     </div>
                 </form>
